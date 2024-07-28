8a66b2ec369397bd3550b271bc0e267907a03c3f
sherif1152 <sfathey606@gmail.com>
2024-07-28 18:29:49 +0000

    first commit

---
+++ robot_description_pkg/launch/robot.launch.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import xacro
+
+
+def generate_launch_description():
+
+    # Get dir names
+    pkg_share = get_package_share_directory('robot_description_pkg')
+    
+
+    # Get rviz config
+    rviz_config = PathJoinSubstitution([
+        FindPackageShare('robot_description_pkg'), 'rviz/view_robot.rviz'
+    ])
+
+    # Add launch description variables
+    namespace = LaunchConfiguration('namespace')
+    spawn_robot = LaunchConfiguration('spawn_robot')
+    # robot_description = LaunchConfiguration('robot_description')
+
+    # Add launch arguments
+    namespace_arg = DeclareLaunchArgument(
+                        name='namespace',
+                        default_value='',
+                        description='Node namespace')
+
+    spawn_robot_arg = DeclareLaunchArgument(
+                    name='spawn_robot',
+                    default_value='true',
+                    description='Flag to spawn the robot or not')
+
+    # Compute robot_description string
+    xacro_file = os.path.join(pkg_share, 'urdf/robot.xacro')
+    robot_description = xacro.process(xacro_file)
+
+    # Define parameters
+    common_params = {
+        'robot_description': robot_description,
+    }
+
+    # Add nodes
+    robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            namespace=namespace,
+            parameters=[common_params])
+
+    joint_state_publisher = Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+            namespace=namespace)
+        #     arguments=[robot_description])
+
+    rviz_node = Node(
+            condition=IfCondition(spawn_robot),
+            package='rviz2',
+            executable='rviz2',
+            name='rviz',
+            output='screen',
+            namespace=namespace,
+            arguments=['-d', rviz_config])
+
+    # Launch description
+    launch_description = LaunchDescription()
+
+    # Add launch actions to the launch description
+    launch_description.add_action(namespace_arg)
+    launch_description.add_action(spawn_robot_arg)
+    launch_description.add_action(joint_state_publisher)
+    launch_description.add_action(robot_state_publisher)
+    launch_description.add_action(rviz_node)
+
+    return launch_description
